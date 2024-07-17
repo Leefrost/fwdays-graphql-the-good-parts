ba16f62a4899a7af25ec981493f75ef9bb380165
Sergii Lischuk <sl@leobit.com>
2024-07-17 20:02:00 +0300

    Added github field to Speaker

--- src/FWDays.Speakers/Database/Speaker.cs
+++ src/FWDays.Speakers/Database/Speaker.cs
@@ -3,14 +3,11 @@
 public class Speaker
 {
     public int Id { get; set; }
-
     public string FirstName { get; set; }
-    
     public string LastName { get; set; }
     public string Company { get; set; }
-
     public string Position { get; set; }
     public string? Bio { get; set; }
-
     public string Topic { get; set; }
+    public string Github { get; set; }
 }
